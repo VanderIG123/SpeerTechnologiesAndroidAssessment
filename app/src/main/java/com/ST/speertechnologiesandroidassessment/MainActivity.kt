7990d50a6ee39a56955df9c025c73dfa90844312
vanderouana <vouana@sfu.ca>
2023-06-05 20:33:01 -0700

    Profile card and followers list section moved to ProfileFragment

--- app/src/main/java/com/ST/speertechnologiesandroidassessment/MainActivity.kt
+++ app/src/main/java/com/ST/speertechnologiesandroidassessment/MainActivity.kt
@@ -2,14 +2,9 @@ package com.ST.speertechnologiesandroidassessment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.activity.viewModels
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.ST.speertechnologiesandroidassessment.databinding.ActivityMainBinding
-import com.ST.speertechnologiesandroidassessment.utils.Utils.hide
-import com.ST.speertechnologiesandroidassessment.utils.Utils.removeFromParent
-import com.ST.speertechnologiesandroidassessment.utils.Utils.show
-import com.bumptech.glide.Glide
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,58 +24,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun initPage() {
         mBinding?.searchBtn?.setOnClickListener {
-            viewModel.fetchGithubProfile(mBinding?.searchTxt?.text.toString())
-        }
-
-        viewModel.githubProfileLiveData.observe(this) {
-            handleGithubProfileResult(it)
+            beginSearch(mBinding?.searchTxt?.text.toString())
         }
     }
 
+    private fun beginSearch(searchKeyword: String) {
+        val profileFragment = ProfileFragment()
+        profileFragment.searchKeyword = searchKeyword
 
-    private fun handleGithubProfileResult(profileState: ProfileState?) {
 
-        when (profileState) {
-            ProfileState.Loading -> mBinding?.progressBar?.show()
-            is ProfileState.Loaded -> showGithubProfileDetails(profileState.profile)
-            ProfileState.NotFound -> showProfileNotFoundMessage()
-            ProfileState.NotLoaded -> {}
-            else -> {}
-        }
+        supportFragmentManager
+            .beginTransaction()
+            .addToBackStack(null)
+            .add(R.id.fragment_container, profileFragment)
+            .commit()
     }
-
-    private fun showGithubProfileDetails(profile: GithubProfile) {
-        mBinding?.progressBar?.removeFromParent()
-        mBinding?.notingTxt?.hide()
-        mBinding?.githubProfileContainer?.show()
-        mBinding?.githubProfileCard?.usernameTxt?.text = profile.email
-        mBinding?.githubProfileCard?.emailTxt?.text = profile.name
-        mBinding?.githubProfileCard?.descriptionTxt?.text = profile.description
-        mBinding?.githubProfileCard?.followersCountTxt?.text =
-            "${getString(R.string.followers)}: ${profile.followerCount}"
-        mBinding?.githubProfileCard?.followingCountTxt?.text =
-            "${getString(R.string.following)}: ${profile.followingCount}"
-
-
-        ////.placeholder(R.drawable.loading_spinner)
-        Glide
-            .with(this)
-            .load(profile.avatarUrl)
-            .into(mBinding?.githubProfileCard?.profileImg!!)
-
-
-        val adapter = ProfileAdapter(profile.followerList)
-        mBinding?.followersList?.layoutManager = LinearLayoutManager(this)
-        mBinding?.followersList?.adapter = adapter
-
-
-    }
-
-    private fun showProfileNotFoundMessage() {
-        mBinding?.progressBar?.removeFromParent()
-        mBinding?.notingTxt?.show()
-        mBinding?.githubProfileContainer?.hide()
-    }
-
 
 }
---
+++ app/src/main/java/com/ST/speertechnologiesandroidassessment/ProfileFragment.kt
@@ -0,0 +1,85 @@
+package com.ST.speertechnologiesandroidassessment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ST.speertechnologiesandroidassessment.databinding.FragmentProfileBinding
+import com.ST.speertechnologiesandroidassessment.utils.Utils.hide
+import com.ST.speertechnologiesandroidassessment.utils.Utils.removeFromParent
+import com.ST.speertechnologiesandroidassessment.utils.Utils.show
+import com.bumptech.glide.Glide
+
+
+class ProfileFragment : Fragment() {
+
+    private val viewModel by viewModels<MainActivityViewModel>()
+    private var mBinding: FragmentProfileBinding? = null
+    var searchKeyword = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = FragmentProfileBinding.inflate(layoutInflater)
+        return mBinding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initPage()
+    }
+
+    private fun initPage() {
+        viewModel.githubProfileLiveData.observe(viewLifecycleOwner) {
+            handleGithubProfileResult(it)
+        }
+
+        viewModel.fetchGithubProfile(searchKeyword)
+
+    }
+
+    private fun handleGithubProfileResult(profileState: ProfileState?) {
+
+        when (profileState) {
+            ProfileState.Loading -> mBinding?.progressBar?.show()
+            is ProfileState.Loaded -> showGithubProfileDetails(profileState.profile)
+            ProfileState.NotFound -> showProfileNotFoundMessage()
+            ProfileState.NotLoaded -> {}
+            else -> {}
+        }
+    }
+
+    private fun showGithubProfileDetails(profile: GithubProfile) {
+        mBinding?.progressBar?.removeFromParent()
+        mBinding?.notingTxt?.hide()
+        mBinding?.githubProfileContainer?.show()
+        mBinding?.githubProfileCard?.usernameTxt?.text = profile.email
+        mBinding?.githubProfileCard?.emailTxt?.text = profile.name
+        mBinding?.githubProfileCard?.descriptionTxt?.text = profile.description
+        mBinding?.githubProfileCard?.followersCountTxt?.text =
+            "${getString(R.string.followers)}: ${profile.followerCount}"
+        mBinding?.githubProfileCard?.followingCountTxt?.text =
+            "${getString(R.string.following)}: ${profile.followingCount}"
+
+
+        Glide
+            .with(this)
+            .load(profile.avatarUrl)
+            .into(mBinding?.githubProfileCard?.profileImg!!)
+
+
+        val adapter = ProfileAdapter(profile.followerList)
+        mBinding?.followersList?.layoutManager = LinearLayoutManager(requireContext())
+        mBinding?.followersList?.adapter = adapter
+    }
+
+    private fun showProfileNotFoundMessage() {
+        mBinding?.progressBar?.removeFromParent()
+        mBinding?.notingTxt?.show()
+        mBinding?.githubProfileContainer?.hide()
+    }
+}
